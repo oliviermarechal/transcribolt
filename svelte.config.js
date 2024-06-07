24160998608b868b65874965b94ab2612e52c3ed
Olivier Maréchal <o.marechal@icloud.com>
2024-06-07 14:25:43 +0200

    Try to fix deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,13 @@
 // import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import adapterStatic from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-node';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapterStatic(),
+		adapter: adapter(),
 	}
 };
 
