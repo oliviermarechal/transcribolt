0b793e671abbf81ce93bef6ad1961b78c56a3aac
Olivier Maréchal <o.marechal@icloud.com>
2024-06-07 11:34:49 +0200

    Fix prod env

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,8 @@ import { defineConfig, loadEnv } from 'vite';
 import dotenvExpand from 'dotenv-expand';
 
 export default defineConfig(({mode}) => {
-	if (mode === 'development') {
-		const env = loadEnv(mode, process.cwd(), '');
-		dotenvExpand.expand({ parsed: env });
-	}
+	const env = loadEnv(mode, process.cwd(), '');
+	dotenvExpand.expand({ parsed: env });
 
 	return {
 		plugins: [sveltekit()],
