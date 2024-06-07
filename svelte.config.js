f714e0fbe3889f0c7db62754e53e865943f93db6
Olivier Maréchal <o.marechal@icloud.com>
2024-06-07 12:07:54 +0200

    Update vite adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,13 @@
-import adapter from '@sveltejs/adapter-vercel';
+// import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapterStatic from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter(),
+		adapter: adapterStatic(),
 	}
 };
 
