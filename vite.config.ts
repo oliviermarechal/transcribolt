aff0bdf6a2f1676a5088c7801500354014c890bc
Olivier Maréchal <o.marechal@icloud.com>
2024-06-08 22:21:57 +0200

    Update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig, loadEnv } from 'vite';
 import dotenvExpand from 'dotenv-expand';
+import path from 'path';
 
 export default defineConfig(({mode}) => {
 	const env = loadEnv(mode, process.cwd(), '');
@@ -8,5 +9,10 @@ export default defineConfig(({mode}) => {
 
 	return {
 		plugins: [sveltekit()],
+		resolve: {
+			alias: {
+				core: path.resolve(__dirname, '../packages/core')
+			}
+		}
 	}
 });
