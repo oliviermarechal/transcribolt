f2bdf8493237e3e90c2b584dfd9ae19b6895987d
Olivier Maréchal <o.marechal@icloud.com>
2024-06-12 10:20:01 +0200

    update dabatase port

--- src/lib/server/app/app.ts
+++ src/lib/server/app/app.ts
@@ -37,7 +37,7 @@ export class App {
                     host: process.env.DB_HOST,
                     user: process.env.DB_USER,
                     password: process.env.DB_PASSWORD,
-                    port: 5434,
+                    port: 5432,
                     max: 10,
                 })
             })
